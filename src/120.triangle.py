e2f16245e6aeb0263ddbda5ed8ca3adeb009b44f
Huy Bui <huy03081999@gmail.com>
2024-05-30 17:57:17 +0700

    add: 120

---
+++ src/120.triangle.py
@@ -0,0 +1,44 @@
+# Level: Medium
+# TAGS: Array, Dynamic Programming
+
+from typing import List
+
+
+class Solution:
+    # DP Top - Down | Time: O(N^2) | Space: O(N^2)
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        memo = {}
+
+        def dfs(i, j):
+            if i == len(triangle):
+                return 0
+            if (i, j) in memo:
+                return memo[(i, j)]
+
+            lower_left = triangle[i][j] + dfs(i + 1, j)
+            lower_right = triangle[i][j] + dfs(i + 1, j + 1)
+
+            memo[(i, j)] = min(lower_left, lower_right)
+            return memo[(i, j)]
+
+        return dfs(0, 0)
+
+    # DP Bottom - Up | Time: O(N^2) | Space: O(N^2)
+    def minimumTotal2(self, triangle: List[List[int]]) -> int:
+        n = len(triangle)
+        dp = [[0] * n for _ in range(n)]
+        dp[n - 1] = triangle[n - 1]
+
+        for i in range(n - 2, -1, -1):
+            for j in range(i + 1):
+                upper_left = triangle[i][j] + dp[i + 1][j]
+                upper_right = triangle[i][j] + dp[i + 1][j + 1]
+                dp[i][j] = min(upper_left, upper_right)
+
+        return dp[0][0]
+
+
+solution = Solution().minimumTotal2
+print("11", solution([[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]]))
+print("-10", solution([[-10]]))
+print("-1", solution([[-1], [2, 3], [1, -1, -3]]))
